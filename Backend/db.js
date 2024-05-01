4d562dd0aba3e56f6e22969b20a8c66d3ac90748
leart <learthyseni12@gmail.com>
2024-05-01 23:58:59 +0200

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -2,8 +2,7 @@ import mysql from "mysql2"
 
 export const db = mysql.createConnection({
     host:"localhost",
-    user:"root",
-    password:"L3@rti123",
+    user:"innsight",
+    password:"leart123",
     database:"innsight"
-
 })
